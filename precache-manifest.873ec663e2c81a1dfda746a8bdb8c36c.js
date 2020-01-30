4ba34f42f0b47b0e130ecff37feb9d14f6e81880
hari <hari.k.krishna4@gmail.com>
2020-01-30 03:06:41 -0600

    Updates

---
+++ precache-manifest.873ec663e2c81a1dfda746a8bdb8c36c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa16eedfb87ac99ed783619e1a6c4a59",
+    "url": "/react-complete-tutorial/index.html"
+  },
+  {
+    "revision": "69b544bf49873f915686",
+    "url": "/react-complete-tutorial/static/css/main.66bb16cc.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/react-complete-tutorial/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-complete-tutorial/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "69b544bf49873f915686",
+    "url": "/react-complete-tutorial/static/js/main.921544cc.chunk.js"
+  },
+  {
+    "revision": "0ee8535e7d4aff741983",
+    "url": "/react-complete-tutorial/static/js/runtime-main.ef82bd43.js"
+  }
+]);
